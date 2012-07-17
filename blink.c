6212439766156a4f20dc5016e1d7172471afaaf7
Luke wilde <luke.wilde@gmail.com>
2012-07-17 09:57:27 +0100

    Improving variable names.

--- blink.c
+++ blink.c
@@ -1,21 +1,18 @@
 /*
   Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
-
-  This example code is in the public domain.
  */
 
-int inputPin = 9;
+int outputPin = 9;
 
 void setup() {
   // initialize the digital pin as an output.
-  // Pin 13 has an LED connected on most Arduino boards:
-  pinMode(inputPin, OUTPUT);
+  pinMode(outputPin, OUTPUT);
 }
 
 void loop() {
-  analogWrite(inputPin, 255);   // set the LED on
-  delay(500);              // wait for a second
-  analogWrite(inputPin, 120);    // set the LED off
-  delay(500);              // wait for a second
+  analogWrite(outputPin, 255);
+  delay(500);
+  analogWrite(outputPin, 120);
+  delay(500);
 }
